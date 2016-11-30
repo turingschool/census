ed0d164f99d1ea14ecb2c271bfa600ea59e2e682
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-11-30 15:51:51 -0700

    Finish acceptance test

--- spec/acceptance/visitor/minimum_signup_spec.rb
+++ spec/acceptance/visitor/minimum_signup_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "user confirms email with minimum user info" do
   scenario "by completing user form" do
-    # visit ''
+    user = FactoryGirl.create(:user)
+    visit "users/edit?token=#{user.token}"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Submit"
 
+    within 'tr' do
+      expect(page).to have_content("#{user.first_name} #{user.last_name}")
+    end
+    expect(page).to have_css('tr', count: 2)
   end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :user do
+    first_name ""
+  end
+end
