96c13dc66cef45aae49372656c8ef44793425c22
PlanetEfficacy <jspevack@gmail.com>
2016-11-29 20:11:21 -0700

    [WIC] initial commit on sign up with email spec

---
+++ spec/features/visitor/sign_up_with_email_spec.rb
@@ -0,0 +1,25 @@
+# As a visitor,
+# When I visit the app
+# I see a form
+# And I enter my name
+# And I enter my email
+# And I click 'send confirmation'
+#
+# Then I see a confirmation message
+# And I receive an email with a confirmation link.
+require 'rails_helper'
+
+RSpec.feature "user signs up for census" do
+  scenario "by initiating email confirmation" do
+    visit sign_up_path
+    expect(page).to have_css('form')
+
+    fill_in 'First name', with: 'Jeff'
+    fill_in 'Last name', with: 'Casimir'
+    fill_in 'Email', with: ENV['JESSE_EMAIL']
+
+    click_button 'Submit'
+
+    expect(page).to have_content("A confirmation email has been sent to #{ENV['JESSE_EMAIL']}.")
+  end
+end
