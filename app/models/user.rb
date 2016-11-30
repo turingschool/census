6c6d468bc3073ea3ed39791070462c84726eb07e
PlanetEfficacy <jspevack@gmail.com>
2016-11-30 15:06:11 -0700

    Add token private method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   validates_presence_of :email
 
   def self.start_to_create(user_params)
-    token = Digest::SHA256.hexdigest(user_params["email"])
     new(  first_name: user_params["first_name"],
           last_name:  user_params["last_name"],
           email:      user_params["email"],
-          token:      token)
+          token:      token(user_params["email"]))
   end
 
-  # add token private method.
+  private
+
+    def self.token(text)
+      Digest::SHA256.hexdigest(text)
+    end
+
 end
