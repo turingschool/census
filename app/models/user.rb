c3f8506ff16fb0eb56deba412f6c6a40933ebc5b
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-11-30 17:15:52 -0700

    Add Devise columns to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
--- db/migrate/20161130034708_create_user.rb
+++ db/migrate/20161130034708_create_user.rb
@@ -3,7 +3,6 @@ class CreateUser < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :token
       t.timestamps
     end
