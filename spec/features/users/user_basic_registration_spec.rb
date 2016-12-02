2b0c5fd3f2a63eabb7e4706e25ccc0150e22c148
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-12-01 20:15:53 -0700

    Add missing first name sad path feature test

--- spec/features/users/user_basic_registration_spec.rb
+++ spec/features/users/user_basic_registration_spec.rb
@@ -19,7 +19,21 @@ RSpec.feature 'User signs up' do
     expect(User.last.first_name).to eq('Jeff')
     expect(User.last.last_name).to eq('Casimir')
     expect(User.last.email).to eq(ENV['MY_EMAIL'])
-    expect(User.last.password).to eq('password')
-    expect(User.last.password_confirmation).to eq('password')
+  end
+
+  scenario 'and does not fill in first name' do
+    visit root_path
+    click_link 'Login'
+    click_link 'Sign up'
+    fill_in 'Last name', with: 'Casimir'
+    fill_in 'Email', with: ENV['MY_EMAIL']
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    error_message = "First name can't be blank"
+
+    expect(page).to have_content(error_message)
+    expect(User.count).to eq(0)
   end
 end
