6d46dcd67ebd4e0164fd8a9d85b7f05b8166b792
PlanetEfficacy <jspevack@gmail.com>
2016-12-04 14:56:02 -0700

    Add acceptance test for user views account info

---
+++ spec/acceptance/user_views_account_info_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'User views account info' do
+  scenario 'by visiting user show page' do
+    user = create(:user)
+    visit root_path
+    click_link 'Login'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Login'
+    click_link 'Account Info'
+
+    expect(page).to have_content("Account Profile")
+    expect(page).to have_content(user.first_name)
+    expect(page).to have_content(user.last_name)
+    expect(page).to have_content(user.email)
+    expect(page).to have_link("Edit profile")
+  end
+end
