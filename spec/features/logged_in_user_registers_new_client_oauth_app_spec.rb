094cfd34a62f4706c8ff558f0d037cb79f0423c4
Calaway <Christopher.Calaway@gmail.com>
2016-12-04 16:48:27 -0700

    Complete feature test Logged In User Registers New Client OAuth App.

--- spec/features/logged_in_user_registers_new_client_oauth_app_spec.rb
+++ spec/features/logged_in_user_registers_new_client_oauth_app_spec.rb
@@ -1,26 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Logged In User Registers New Client OAuth App", type: :feature do
-  describe "they get a client id and client secret" do
-    #s a Registered User
-    user = User.create(first_name: 'Bryan', last_name: 'Goss', email: ENV['MY_EMAIL'])
-    #n order to use OAuth with Census
-    #hen I visit the Register App page
-    visit new_oauth_app_path
-    #nd I fill in App Name
-    fill_in 'oauth_app[name]', with: 'Thingy'
-    #nd I fill in App Callback URL
-    fill_in 'oauth_app[callback_url]', with: 'http://localhost:3000/auth/census/callback'
-    #nd I click Submit
-    click_on 'Register Application'
-    #hen I will see my Application's Page
-    #nd I will see my Application's Name
-    expect(page).to have_content('Application Name: Thingy')
-    #nd I will see my Client ID
-    expect(page).to have_content('Client ID: 2')
-    #nd my Client Secret
-    expect(page).to have_content('Client Secret: 1')
-    #nd my Application's Callback URL.
-    expect(page).to have_content('Callback URL: http://localhost:3000/auth/census/callback')
+  it "they get a client id and client secret" do
+    visit new_oauth_application_path
+
+    fill_in 'doorkeeper_application[name]', with: 'Thingy'
+    fill_in 'doorkeeper_application[redirect_uri]', with: 'https://localhost:3000/auth/census/callback'
+    click_on 'Submit'
+
+    expect(page).to have_content('Application: Thingy')
+    expect(page).to have_content('Application Id: ')
+    expect(page).to have_content('Secret: ')
+    expect(page).to have_content('Callback urls: https://localhost:3000/auth/census/callback')
   end
 end
