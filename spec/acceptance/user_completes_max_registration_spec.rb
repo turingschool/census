80eec329b94f8e687825825faaac82a51fb1bfaf
PlanetEfficacy <jspevack@gmail.com>
2016-12-05 13:14:45 -0700

    Add paramters to configure_permitted_params in devise

--- spec/acceptance/user_completes_max_registration_spec.rb
+++ spec/acceptance/user_completes_max_registration_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'User signs up' do
     click_link 'Login'
     click_link 'Sign up'
     fill_in 'First name', with: user[:first_name]
-    fill_in 'Last name', with: user[:lsat_name]
+    fill_in 'Last name', with: user[:last_name]
     fill_in 'Email', with: user[:email]
     fill_in 'Password', with: user[:password]
     fill_in 'Password confirmation', with: user[:password]
@@ -16,7 +16,7 @@ RSpec.feature 'User signs up' do
     fill_in 'GitHub', with: user[:git_hub]
     fill_in 'Slack', with: user[:slack]
     find("option[value='1606']").select_option
-    find("Armstrong").click
+    find("#armstrong").click
 
     click_button 'Sign up'
 
