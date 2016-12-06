ee86a4c6bc243bc7ec142c509d96a773c23a2ab7
Bryan Goss <bcgoss@gmail.com>
2016-12-06 16:34:39 -0700

    change doorkeeper config to use current_user

--- spec/features/oauth/user_signs_in_from_oauth_url_spec.rb
+++ spec/features/oauth/user_signs_in_from_oauth_url_spec.rb
@@ -15,13 +15,15 @@ RSpec.describe 'OAuth Access Grant' do
   it 'displays grant information to authenticated users' do
     #As a registered user
     user = create :user
-    login(user)
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'Log in'
     #In order to grant access to an oauth client application
     application = create :oauth_application, name: 'Test App'
     #When I visit the authorization path
     visit "/oauth/authorize?client_id=#{application.uid}&response_type=code&redirect_uri=#{application.redirect_uri}"
     #Then I should see the client app;ication's information
-save_and_open_page
     expect(page).to have_content 'Test App'
     #And I should see an option to grant access
     #And I should see an option to deny access
