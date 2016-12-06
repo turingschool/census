a1c0f76a96e64b283addd1cb2e94de07e77a5bc5
PlanetEfficacy <jspevack@gmail.com>
2016-12-05 17:35:13 -0700

    initial commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ RSpec.configure do |config|
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  # config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
   if config.files_to_run.one?
     config.default_formatter = 'doc'
