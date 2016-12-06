0050300d4a9ec8b30dcf6c9e4197f4a37dd6a146
PlanetEfficacy <jspevack@gmail.com>
2016-12-06 10:09:01 -0700

    Add affiliations to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206170527) do
+ActiveRecord::Schema.define(version: 20161206170737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "affiliations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_affiliations_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_affiliations_on_user_id", using: :btree
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -88,6 +97,8 @@ ActiveRecord::Schema.define(version: 20161206170527) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "affiliations", "groups"
+  add_foreign_key "affiliations", "users"
   add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
   add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
 end
