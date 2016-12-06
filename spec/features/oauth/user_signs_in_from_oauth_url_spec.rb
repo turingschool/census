7055b550b1fbf4552088ba92670eb0af5ae2d3d6
Bryan Goss <bcgoss@gmail.com>
2016-12-06 08:41:47 -0700

    Revert "Add test for logged in user visits authorization path"
    This reverts commit 9fb9fb570e40f9029b7608e731c5607fe9ee7f37.

--- spec/features/oauth/user_signs_in_from_oauth_url_spec.rb
+++ spec/features/oauth/user_signs_in_from_oauth_url_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'OAuth Access Grant' do
   it 'redirects to login for unauthenticated users' do
-pending
     #As a not-signed-in registered user
     user = create :user
     #In order to grant access to an oauth client application
@@ -11,22 +10,12 @@ pending
     visit "/oauth/authorize?client_id=#{application.uid}&response_type=code&redirect_uri=#{application.redirect_uri}"
     #Then I should see a log in page.
     expect(page).to have_content 'Log in'
-
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    click_on 'Log in'
-    expect(current_path).to eq(oauth_authorization_path)
-    expect(page).to have_content 'Test App'
   end
 
   it 'displays grant information to authenticated users' do
     #As a registered user
     user = create :user
-#    login(user)
-    visit new_user_session_path
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    click_on 'Log in'
+    login(user)
     #In order to grant access to an oauth client application
     application = create :oauth_application, name: 'Test App'
     #When I visit the authorization path
