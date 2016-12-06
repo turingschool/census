4087089f63f4ca26129f68d8044e7fd8dea044a8
PlanetEfficacy <jspevack@gmail.com>
2016-12-06 10:19:38 -0700

    Add groups have many users through affiliations

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   it { should have_many(:affiliations).dependent(:destroy) }
+  it { should have_many(:users).through(:affiliations) }
 end
