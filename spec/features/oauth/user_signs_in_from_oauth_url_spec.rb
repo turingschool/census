1494f1d01c649ed6e1be93105ac1b161408853a3
Bryan Goss <bcgoss@gmail.com>
2016-12-05 19:14:37 -0700

    Finish test for unauthenticated user granting access to app

--- spec/features/oauth/user_signs_in_from_oauth_url_spec.rb
+++ spec/features/oauth/user_signs_in_from_oauth_url_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe 'OAuth Access Grant' do
     #Then I should see a log in page.
     expect(page).to have_content 'Log in'
   end
+
+  it 'displays grant information to authenticated users' do
+    #As a registered user
+    user = create :user
+    login(user)
+    #In order to grant access to an oauth client application
+    application = create :oauth_application, name: 'Test App'
+    #When I visit the authorization path
+    visit "/oauth/authorize?client_id=#{application.uid}&response_type=code&redirect_uri=#{application.redirect_uri}"
+    #Then I should see the client app;ication's information
+save_and_open_page
+    expect(page).to have_content 'Test App'
+    #And I should see an option to grant access
+    #And I should see an option to deny access
+  end
 end
