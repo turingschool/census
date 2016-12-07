bb9567543069f9deaa16b3eb56a55647a287aef5
PlanetEfficacy <jspevack@gmail.com>
2016-12-06 17:50:37 -0700

    Add feature user can create affiliation to group

--- spec/acceptance/user_creates_affiliations_spec.rb
+++ spec/acceptance/user_creates_affiliations_spec.rb
@@ -4,16 +4,17 @@ RSpec.feature 'User creates affiliations' do
   scenario 'by completing the my affiliations form' do
     user = create(:user)
     create(:group)
+    create(:group, name: "LGBTTuring")
     login(user)
 
-    visit new_affiliation_path
-    save_and_open_page
-    find("#affiliation_armstrong").click
-    click_button "Submit"
+    click_link "Account Info"
+    click_link "Edit affiliations"
+    find("#1").set(true)
+    click_button "Save changes"
 
     expect(current_path).to eq(user_path(user))
     within "div#affiliations" do
-      expect(page).to have_content("armstrong")
+      expect(page).to have_content("Armstrong")
     end
   end
 end
