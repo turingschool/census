495e6ba9ec5a29519b8b0787a3d102468ef75a64
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-12-07 11:49:31 -0700

    Add Devise methods to ConfirmationMailer class

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,2 +1,5 @@
-class ConfirmationMailer < ApplicationMailer
+class ConfirmationMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 end
