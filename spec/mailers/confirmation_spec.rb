263bb2ab71d3cb6ac9c45b1ecab24d2cc9fe663b
Calaway <Christopher.Calaway@gmail.com>
2016-12-08 21:18:49 -0700

    Work in progress on email confirmation test.

--- spec/mailers/confirmation_spec.rb
+++ spec/mailers/confirmation_spec.rb
@@ -1,25 +1,37 @@
 require "rails_helper"
 
 RSpec.describe ConfirmationMailer, type: :mailer do
-    before(:all) do
-      setup_mailer
-      Devise.mailer = 'Devise::Mailer'
-      Devise.mailer_sender = 'test@example.com'
+  before do
+    ActionMailer::Base.deliveries = []
+    Devise.mailer = 'Devise::Mailer'
+# change this address?
+    Devise.mailer_sender = 'test@example.com'
+  end
+
+  after do
+    Devise.mailer = 'Devise::Mailer'
+# change this address?
+    Devise.mailer_sender = 'please-change-me@config-initializers-devise.com'
+  end
+
+  def user
+    @user ||= User.create!(attributes_for(:user))
+  end
+
+  def mail
+    @mail ||= begin
+      user
+      ActionMailer::Base.deliveries.first
     end
+  end
 
-    describe "confirmation email" do
-      expect(2).to eq(3)
+  # let(:mail) { ActionMailer::Base.deliveries.first }
 
-  #   let(:mail) { Notifications.signup }
-  #
-  #   it "renders the headers" do
-  #     expect(mail.subject).to eq("Signup")
-  #     expect(mail.to).to eq(["to@example.org"])
-  #     expect(mail.from).to eq(["from@example.com"])
-  #   end
-  #
-  #   it "renders the body" do
-  #     expect(mail.body.encoded).to match("Hi")
-  #   end
+  it "sends email after creating the user" do
+    expect(mail).to_not eq(nil)
+
+    # expect(mail.subject).to eq("Signup")
+    # expect(mail.to).to eq(["to@example.org"])
+    # expect(mail.from).to eq(["from@example.com"])
   end
 end
