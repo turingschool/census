c239f671f67681426ca58cb4947dfd0ef3223a7d
Calaway <Christopher.Calaway@gmail.com>
2016-12-08 21:18:12 -0700

    Remove Devise test helpers as they aren't necessary at this time.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,8 +21,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   # config.formatter = :documentation
-  # config.include Devise::Test::ControllerHelpers, type: :controller
-  # config.include Devise::Test::ControllerHelpers, type: :view
 end
 
 def login(user)
