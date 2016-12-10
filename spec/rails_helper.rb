dceac3cecdcf656e3fc217cd1fb85c277ebd1105
Calaway <Christopher.Calaway@gmail.com>
2016-12-10 15:56:10 -0700

    Add Devise controller test helpers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.formatter = :documentation
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
 
 def login(user)
