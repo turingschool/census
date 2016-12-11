6196d699d029a67622c44fa81c1efa5942216689
PlanetEfficacy <jspevack@gmail.com>
2016-12-10 17:29:36 -0700

    Remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_filter :store_current_location, :unless => :devise_controller?
   before_action :check_authenticity, if: :request_from_client?
-  #
-  def request_from_client?
-    !params[:client_id].nil? # and it is a valid id
-    # if it's not from a client
-    # is there a session record of the redirect uri
-    # and if so, delete it.
-  end
-
-  def check_authenticity
-    if authentic_client_id?
-      store_current_location
-    else
-      head :forbidden
-    end
-  end
-
-  def store_current_location
-    session[:return_path] = request.fullpath
-  end
-
-  def authentic_client_id?
-    Doorkeeper::Application.by_uid(params[:client_id])
-  end
-
- #  def after_sign_in_path_for(resource)
- #    stored_location_for(resource) ||
- #    if resource.is_a?(User) && resource.can_publish?
- #       publisher_url
- #     else
- #       super
- #     end
- # end
 
   protected
 
@@ -47,4 +14,25 @@ class ApplicationController < ActionController::Base
                                                         :slack,
                                                         :cohort ])
   end
+
+  private
+    def request_from_client?
+      !params[:client_id].nil?
+    end
+
+    def check_authenticity
+      if authentic_client_id?
+        store_current_location
+      else
+        head :forbidden
+      end
+    end
+
+    def store_current_location
+      session[:return_path] = request.fullpath
+    end
+
+    def authentic_client_id?
+      Doorkeeper::Application.by_uid(params[:client_id])
+    end
 end
--- spec/features/doorkeeper_reroutes_back_to_client_app_spec.rb
+++ spec/features/doorkeeper_reroutes_back_to_client_app_spec.rb
@@ -3,29 +3,16 @@ require 'rails_helper'
 RSpec.describe "OAuth flow from client app" do
   scenario "redirects back to client app affter successful login" do
     application = create(:oauth_application)
-    # oauth = {
-    #   client_id: application.uid,
-    #   redirect_uri: application.redirect_uri,
-    #   response_type: "code"
-    # }
-
-    # allow_any_instance_of(ApplicationController).to receive(:client_id).and_return(true)
-
     user = create(:user)
     visit oauth_authorization_path( client_id: application.uid,
                                     redirect_uri: application.redirect_uri,
                                     response_type: "code")
-    # visit oauth_authorization_path
-    # visit "/oauth/applications?client_id=#{application.uid}&redirect_uri=#{application.redirect_uri}&response_type=code"
 
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Log in"
 
     expect(current_path).to eq(oauth_authorization_path)
-    # click_button "Authorize"
-    #
-    # expect(current_path).to eq(application.redirect_uri)
   end
 
   scenario "renders 404 if the client app is not recognized" do
@@ -37,6 +24,4 @@ RSpec.describe "OAuth flow from client app" do
 
     expect(status_code).to eq(403)
   end
-
-
 end
