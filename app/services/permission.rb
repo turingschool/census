145806ac5109632905d642f4158b74b6d53fe556
Calaway <calaway@users.noreply.github.com>
2016-12-11 19:07:22 -0700

    Build permissions service.

---
+++ app/services/permission.rb
@@ -0,0 +1,18 @@
+class Permission
+  def self.authorized?(user, controller, action)
+    if user
+      return true if controller == "users" && action.in?(%w(index show edit update))
+      return true if controller == "home" && action.in?(%w(index))
+      return true if controller == "affiliations" && action.in?(%w(create new))
+      return true if controller == "devise/sessions" && action.in?(%w(create destroy))
+      return true if controller == "doorkeeper/authorizations" && action.in?(%w(new))
+      false
+    else
+      return true if controller == "home" && action.in?(%w(index))
+      return true if controller == "devise/sessions" && action.in?(%w(new))
+      return true if controller == "devise/registrations" && action.in?(%w(new create))
+      return true if controller == "doorkeeper/authorizations" && action.in?(%w(new))
+      false
+    end
+  end
+end
