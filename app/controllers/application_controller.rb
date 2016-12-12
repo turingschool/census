168fc5e438c24f190d4a01a6183e76cef442e77c
Calaway <calaway@users.noreply.github.com>
2016-12-11 19:04:42 -0700

    Prepare application controller for permission service.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authorize!
 
   protected
+
+  def authorize!
+    unless Permission.authorized?(current_user, params[:controller], params[:action])
+# DEBUGGING ONLY
+puts "**********
+CHECK PERMISSIONS
+Current User: #{!!current_user}
+Controller: #{params[:controller]}
+Action: #{params[:action]}
+**********" #unless RAILS_ENV == "production"
+      render file: "/public/404", status: 404, layout: false
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name,
