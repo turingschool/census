a26ea0165261c717d5d3badfc324bd19cda49fa6
Calaway <calaway@users.noreply.github.com>
2016-12-11 19:51:59 -0700

    Build doorkeeper applications controller tests for visitor.

--- spec/controllers/doorkeeper/applications_controller_spec.rb
+++ spec/controllers/doorkeeper/applications_controller_spec.rb
@@ -2,44 +2,44 @@ require 'rails_helper'
 
 RSpec.describe Doorkeeper::ApplicationsController, type: :controller do
   context "Visitor navigates to" do
+    it "applications#new, the page is rendered successfully" do
+      post :new
+
+      expect(response.status).to eq(404)
+    end
+
+    it "applications#create, they are redirected" do
+      post :create, doorkeeper_application: {name: "Monocle", redirect_uri: "https://localhost:3000/auth/census/callback", scopes: ""}
+
+      expect(response.status).to eq(404)
+    end
+
     it "applications#index, they receive 404 page" do
       post :index
 
       expect(response.status).to eq(404)
     end
 
-    it "applications#create, they receive 404 page" do
-      post :create
-
-      expect(response.status).to eq(404)
-    end
-
-    it "applications#new, they receive 404 page" do
-      post :new
-
-      expect(response.status).to eq(404)
-    end
-
     it "applications#edit, they receive 404 page" do
-      post :edit
+      get :edit, params: {id: 1}
 
       expect(response.status).to eq(404)
     end
 
     it "applications#show, they receive 404 page" do
-      post :show
+      get :show, params: {id: 1}
 
       expect(response.status).to eq(404)
     end
 
     it "applications#update, they receive 404 page" do
-      post :update
+      post :update, params: {id: 1, doorkeeper_application: {name: "Monocle"}}
 
       expect(response.status).to eq(404)
     end
 
     it "applications#destory, they receive 404 page" do
-      post :destroy
+      delete :destroy, params: {id: 1}
 
       expect(response.status).to eq(404)
     end
