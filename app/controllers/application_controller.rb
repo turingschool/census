df6a25f6d2b9bb95ce28e5a49e5185d0a125dc28
PlanetEfficacy <jspevack@gmail.com>
2016-12-12 08:43:14 -0700

    Add doorkeeper controller and remove unused doorkeeper controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :check_authenticity, if: :request_from_client?
 
   protected
 
@@ -14,25 +13,4 @@ class ApplicationController < ActionController::Base
                                                         :slack,
                                                         :cohort ])
   end
-
-  private
-    def request_from_client?
-      !params[:client_id].nil?
-    end
-
-    def check_authenticity
-      if authentic_client_id?
-        store_current_location
-      else
-        head :forbidden
-      end
-    end
-
-    def store_current_location
-      session[:return_path] = request.fullpath
-    end
-
-    def authentic_client_id?
-      Doorkeeper::Application.by_uid(params[:client_id])
-    end
 end
