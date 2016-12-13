e6c64f35e70278d2b129216f9e86dcd0220ebfdb
PlanetEfficacy <jspevack@gmail.com>
2016-12-13 12:30:05 -0700

    Add user_roles migration

--- db/migrate/20161213192814_create_role.rb
+++ db/migrate/20161213192814_create_role.rb
@@ -2,6 +2,7 @@ class CreateRole < ActiveRecord::Migration[5.0]
   def change
     create_table :roles do |t|
       t.string :name
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206170737) do
+ActiveRecord::Schema.define(version: 20161213192814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 20161206170737) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
