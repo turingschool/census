17caaa362df2746fd1a702afa58064d15ed81e75
Calaway <calaway@users.noreply.github.com>
2016-12-14 07:40:53 -0700

    Create route for users index api endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:index]
+      
       get '/user', to: 'credentials#show'
     end
   end
