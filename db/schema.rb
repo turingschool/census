b50641775272abfb96ed01c8a3a92a8a764e64d6
Calaway <calaway@users.noreply.github.com>
2016-12-13 20:03:16 -0700

    Migration to add image to user table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206170737) do
+ActiveRecord::Schema.define(version: 20161214024827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20161206170737) do
     t.string   "twitter"
     t.string   "linked_in"
     t.string   "git_hub"
+    t.string   "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
