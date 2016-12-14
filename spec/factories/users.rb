e6ed56d1517fe3e8e54a38910f9e5e01e0c7813b
Calaway <calaway@users.noreply.github.com>
2016-12-14 07:39:12 -0700

    Add image to user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    image { "https://robohash.org/#{first_name}#{last_name}" }
     password "password"
     confirmed_at DateTime.new()
   end
