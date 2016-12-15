1ffffbbad90f530a471f801f913efcee896b55bd
Calaway <calaway@users.noreply.github.com>
2016-12-14 17:41:57 -0700

    Configure user model for paperclip/aws.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ApplicationRecord
   has_many :affiliations, dependent: :destroy
   has_many :groups, through: :affiliations
 
+  # paperclip configuration
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   def full_name
     "#{first_name} #{last_name}"
   end
