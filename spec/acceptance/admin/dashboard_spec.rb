9d4d0fdedc242f32e8e29f59225d32cd5d91ba61
PlanetEfficacy <jspevack@gmail.com>
2016-12-14 18:32:10 -0700

    Reorganize acceptance tests

--- spec/acceptance/admin/dashboard_spec.rb
+++ spec/acceptance/admin/dashboard_spec.rb
@@ -1,9 +1,3 @@
-# As a logged in admin
-# When I visit the invite dashboard
-# Then I see a link to bulk invite users
-# And I see a list of pending invitations
-# And each invite has an option to resend
-# And each invite has an option to cancel
 require 'rails_helper'
 
 RSpec.feature "Admin dashboard" do
