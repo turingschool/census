e967a432680ae52e6faa71bb41cf738cf4fff96f
Calaway <calaway@users.noreply.github.com>
2016-12-18 17:05:27 -0700

    Remove robohash image from user factory. Add cohort.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
+  possible_cohorts = %w(1602 1603 1605 1606 1608 1610 1611 1701 1703)
+
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    image { "https://robohash.org/#{first_name}#{last_name}" }
+    cohort { possible_cohorts.sample }
     password "password"
     confirmed_at DateTime.new()
   end
