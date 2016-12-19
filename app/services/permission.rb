f1d52811578c94d926bde40b492aab7d2cbe7b8e
Calaway <calaway@users.noreply.github.com>
2016-12-19 07:41:27 -0700

    Refactor permission service.

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -1,5 +1,9 @@
 class Permission
-  def self.authorized?(user, controller, action)
+  def initialize(user)
+    @user ||= User.new
+  end
+
+  def authorized?(user, controller, action)
     if user && user.roles.include?(Role.find_by(name: "admin"))
       return true if controller == "invitations" && action.in?(%w(new create update destroy))
       return true if controller == "admin/dashboard" && action.in?(%w(show))
@@ -15,7 +19,6 @@ class Permission
       return true if controller == "doorkeeper/authorizations" && action.in?(%w(new))
       return true if controller == "doorkeeper/applications" && action.in?(%w(new create index show edit update destroy))
       return true if controller == "users/sessions" && action.in?(%w(create destroy))
-      false
     else
       return true if controller == "home" && action.in?(%w(index))
       return true if controller == "devise/sessions" && action.in?(%w(new))
@@ -23,9 +26,20 @@ class Permission
       return true if controller == "users/registrations" && action.in?(%w(new create))
       return true if controller == "doorkeeper/authorizations" && action.in?(%w(new))
       return true if controller == "users/sessions" && action.in?(%w(new create))
-# TODO: temp: add requirement later
-return true if controller == "api/v1/users" && action.in?(%w(index))
-      false
+      # TODO: This permission is temporary. Add requirement later.
+      return true if controller == "api/v1/users" && action.in?(%w(index))
+    end
+      print_warning(user, controller, action)
+  end
+
+  def print_warning(user, controller, action)
+    unless ENV["RAILS_ENV"] == "production"
+      puts "**********
+      CHECK PERMISSIONS
+      Current User: #{!!user}
+      Controller: #{controller}
+      Action: #{action}
+      **********"
     end
   end
 end
