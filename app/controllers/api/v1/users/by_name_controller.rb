3c03f6c9cdc97fbc82e144df0e3540dbd98d3779
Calaway <calaway@users.noreply.github.com>
2016-12-21 16:02:18 -0700

    Refactor user api controllers to utilize serializer.

--- app/controllers/api/v1/users/by_name_controller.rb
+++ app/controllers/api/v1/users/by_name_controller.rb
@@ -2,15 +2,6 @@ class Api::V1::Users::ByNameController < ApplicationController
   def index
     users = User.search_by_name(params[:q])
 
-    users_data = users.map do |user|
-      {
-        "first_name"=>user.first_name,
-        "last_name"=>user.last_name,
-        "cohort"=>user.cohort,
-        "image_url"=>user.image.url
-      }
-    end
-
-    render json: users_data, status: 200
+    render json: users, each_serializer: UserSerializer, status: 200
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,5 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    users_data = User.all.map do |user|
-      {
-        "first_name"=>user.first_name,
-        "last_name"=>user.last_name,
-        "cohort"=>user.cohort,
-        "image_url"=>user.image.url
-      }
-    end
-
-    render json: users_data
+    render json: User.all
   end
 end
