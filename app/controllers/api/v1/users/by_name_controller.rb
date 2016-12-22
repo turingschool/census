7959244870c1e3faa8593194fed891f7fc6f15c9
Calaway <calaway@users.noreply.github.com>
2016-12-22 07:59:03 -0700

    Send root_url from api controllers to serializer.

--- app/controllers/api/v1/users/by_name_controller.rb
+++ app/controllers/api/v1/users/by_name_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Users::ByNameController < ApplicationController
   def index
     users = User.search_by_name(params[:q])
 
-    render json: users, each_serializer: UserSerializer, status: 200
+    render json: users, each_serializer: UserSerializer, root_url: root_url, status: 200
   end
 end
