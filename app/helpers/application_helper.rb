c8210380bcffd4480208823d1d41a2424f405b41
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-12-21 18:14:19 -0700

    Add link for admins to find dashboard from navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def invited?
     !!session[:invitation_code]
   end
+
+  def user_is_admin?
+    if current_user
+      current_user.roles.where(name: 'admin').exists?
+    end
+  end
 end
