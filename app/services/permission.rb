671b470854800453730a19f362e32869f1f456e1
Bryan Goss <bcgoss@gmail.com>
2016-12-21 17:36:09 -0700

    Add styling and permissions

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -9,7 +9,11 @@ class Permission
       return true if controller == "admin/dashboard" && action.in?(%w(show))
       return true if controller == 'users/sessions' && action.in?(%w(create destroy))
       return true if controller == 'home' && action.in?(%w(index))
+      return true if controller == 'affiliations' && action.in?(%w(create new))
       return true if controller == 'users' && action.in?(%w(index show edit update))
+      return true if controller == "doorkeeper/authorizations" && action.in?(%w(new))
+      return true if controller == "doorkeeper/applications" && action.in?(%w(new create index show edit update destroy))
+      return true if controller == 'doorkeeper/authorized_applications' && action.in?(%w(index destroy))
     elsif user
       return true if controller == "users" && action.in?(%w(index show edit update))
       return true if controller == "home" && action.in?(%w(index))
@@ -19,6 +23,7 @@ class Permission
       return true if controller == "doorkeeper/authorizations" && action.in?(%w(new))
       return true if controller == "doorkeeper/applications" && action.in?(%w(new create index show edit update destroy))
       return true if controller == "users/sessions" && action.in?(%w(create destroy))
+      return true if contrller == 'doorkeeper/authorized_applications' && action.in?(%w(index destroy))
     else
       return true if controller == "home" && action.in?(%w(index))
       return true if controller == "devise/sessions" && action.in?(%w(new))
