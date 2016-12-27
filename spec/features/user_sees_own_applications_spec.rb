79b22fd202d644b55fc19dd86b0f78f6179e7e64
Bryan Goss <bcgoss@gmail.com>
2016-12-27 12:23:54 -0500

    add test to demonstrate new functionality

---
+++ spec/features/user_sees_own_applications_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper' 
+
+RSpec.describe 'Oauth Applications' do
+  it 'index shows a list of my applications' do
+    me = create :user
+    sign_in me
+    my_application = create :oauth_application, owner: me
+    
+    visit oauth_application_path(my_application)
+
+    expect(page).to have_content my_application.name
+  end
+
+  it 'are not visible to other users' do
+    me = create :user
+    sign_in me
+    other = create :user
+    app = create :oauth_application, owner: other
+
+    visit oauth_applications_path
+
+    expect(page).to_not have_content app.name
+  end
+end
