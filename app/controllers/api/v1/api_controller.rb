aefeb8c3ed64b3dba19819a961f01d88e8abcf6c
Calaway <calaway@users.noreply.github.com>
2016-12-28 18:45:05 -0700

    Add protect from forgery to Api::V1::ApiController for security.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
   class ApiController < ActionController::Base
+    protect_from_forgery with: :null_session
+
     private
       def current_resource_owner
         User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
