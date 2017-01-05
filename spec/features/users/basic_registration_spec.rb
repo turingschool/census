ec76a8733627eb6801317683206d3c58c65823c9
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-05 14:32:22 -0700

    Change sign in url for basic registration tests

--- spec/features/users/basic_registration_spec.rb
+++ spec/features/users/basic_registration_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'User signs up' do
   scenario 'by completing minimum registration' do
-    visit root_path
-    click_link 'Login'
-    click_link 'Sign up'
+    role = create :role, name: 'Staff'
+    invite = create :invitation, role: role
+
+    visit invite.generate_url(new_user_registration_url)
+
     fill_in 'First name', with: 'Jeff'
     fill_in 'Last name', with: 'Casimir'
     fill_in 'Email', with: ENV['MY_EMAIL']
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
+
+    save_and_open_page
 
     help_message = 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
 
@@ -22,9 +26,11 @@ RSpec.feature 'User signs up' do
   end
 
   scenario 'and does not fill in first name' do
-    visit root_path
-    click_link 'Login'
-    click_link 'Sign up'
+    role = create :role, name: 'Staff'
+    invite = create :invitation, role: role
+
+    visit invite.generate_url(new_user_registration_url)
+
     fill_in 'Last name', with: 'Casimir'
     fill_in 'Email', with: ENV['MY_EMAIL']
     fill_in 'Password', with: 'password'
