9d7ab018349bbcfd142f3ac2ab3f71f7b88049e1
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-05 15:10:35 -0700

    Fix user max and min registration field acceptance tests

--- spec/acceptance/user/completes_min_registration_spec.rb
+++ spec/acceptance/user/completes_min_registration_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'User signs up' do
   scenario 'by completing minimum registration' do
-    visit root_path
-    click_link 'Sign up'
+    user = attributes_for(:user)
+    role = create :role, name: 'Enrolled'
+    invite = create :invitation, role: role
+
+    visit invite.generate_url(new_user_registration_url)
+
     fill_in 'First name', with: 'Jeff'
     fill_in 'Last name', with: 'Casimir'
-    fill_in 'Email', with: ENV['MY_EMAIL']
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
 
-    help_message = 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
+    help_message = 'You have succesfully signed up! Please log in to continue.'
     expect(page).to have_content(help_message)
   end
 end
