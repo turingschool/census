1f142551ec3230df6ed7b7c9660da0e9996d097d
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-05 12:02:23 -0700

    Remove sign up link from login view

--- spec/features/guests/uninvited_guest_cannot_sign_up_spec.rb
+++ spec/features/guests/uninvited_guest_cannot_sign_up_spec.rb
@@ -6,4 +6,10 @@ RSpec.feature 'Uninvited user features' do
 
     expect(page).to have_content("The page you were looking for doesn't exist.")
   end
+
+  it 'can see login page' do
+    visit root_path
+
+    expect(page).to have_content('Login')
+  end
 end
