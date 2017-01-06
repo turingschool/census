79ae0550ffacc8841ca7c25badde7edac83ea181
PlanetEfficacy <jspevack@gmail.com>
2017-01-05 21:51:11 -0700

    Add bounce update from webhook

--- app/controllers/api/v1/send_grid/events_controller.rb
+++ app/controllers/api/v1/send_grid/events_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::SendGrid::EventsController < Api::V1::ApiController
   def update
-    byebug
-    invitation = Invitation.find_by(email: params[:email])
-    invitation.status = params[:event]
+    webhook_data = JSON.parse(request.body.read)
+    webhook_data.each do |data|
+      invitation = Invitation.find_by(email: data["email"])
+      invitation.status = data["event"] if invitation
+      invitation.save
+    end
   end
 end
--- spec/requests/api/v1/send_grid/events_spec.rb
+++ spec/requests/api/v1/send_grid/events_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe Api::V1::SendGrid::EventsController do
   context "Post request is sent to SendGrid" do
     it "updates status of bounced email" do
-      invitation = create :invitation, email: "email@example.com", status: "mailed"
-      headers = { "CONTENT_TYPE" => "application/json" }
-      post_body = JSON.generate([{"event":"bounce", "email":"email@example.com"}])
-      post '/api/v1/sendgrid/events', post_body
+      create :invitation, email: "email@example.com", status: "mailed"
+      post_body = '[{"event":"bounce", "email":"email@example.com"}]'
+      post '/api/v1/sendgrid/events', post_body, { format: 'json' }
 
-      expect(invitation.bounced?).to eq(true)
-      expect(response.status).to eq(200)
+      expect(Invitation.first.bounce?).to eq(true)
+      expect(response.status).to eq(204)
     end
   end
 end
