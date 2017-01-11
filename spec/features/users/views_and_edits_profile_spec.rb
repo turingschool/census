12c06d186dda12f3d44a3c7dc429ced0ddcb8278
Calaway <calaway@users.noreply.github.com>
2017-01-11 12:54:45 -0700

    Add test for twitter input error styling.

--- spec/features/users/views_and_edits_profile_spec.rb
+++ spec/features/users/views_and_edits_profile_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User visits edit profile page' do
+RSpec.describe 'User visits edit profile page', js: :true do
   it 'has an edit button' do
     me = create :user
     login me
@@ -17,30 +17,20 @@ RSpec.describe 'User visits edit profile page' do
     expect(page).to have_link('Change password')
   end
 
-  xscenario "they can edit their information" do
-    user = User.create(first_name: "OriginalFirst",
-                       last_name:  "OriginalLast",
-                       email:      "Original@Email",
-                       twitter:    "OriginalTwitter",
-                       linked_in:  "OriginalLinkedIn",
-                       git_hub:    "OriginalGitHub",
-                       slack:      "OriginalSlack",
-                       cohort:     "OriginalCohort",
-                       password:   "OriginalPassword")
-    login user
+  context "they enter invalid characters for twitter" do
+    scenario "that input field turns red" do
+      me = create :user
+      login me
 
-    visit user_path user
-    click_on "Edit profile"
-    fill_in "first_name", with: "NewFirst"
+      visit edit_user_path me
 
-    expect(user.first_name).to eq("NewFirst")
-    expect(user.last_name).to  eq("NewLast")
-    expect(user.email).to      eq("New@Email")
-    expect(user.twitter).to    eq("NewTwitter")
-    expect(user.linked_in).to  eq("NewLinkedIn")
-    expect(user.git_hub).to    eq("NewGitHub")
-    expect(user.slack).to      eq("NewSlack")
-    expect(user.cohort).to     eq("NewCohort")
-    expect(user.password).to   eq("NewPassword")
+      fill_in "user[twitter]", with: "j3"
+
+      expect(page).to_not have_css(".input-field-error")
+
+      fill_in "user[twitter]", with: "@j3"
+
+      expect(page).to have_css(".input-field-error")
+    end
   end
 end
