be0561bc294f523726abcb90371d88ae5dd17d66
Calaway <calaway@users.noreply.github.com>
2017-01-11 12:05:12 -0700

    Work on edit user spec.

--- spec/features/users/views_and_edits_profile_spec.rb
+++ spec/features/users/views_and_edits_profile_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User Profile' do
+RSpec.describe 'User visits edit profile page' do
   it 'has an edit button' do
     me = create :user
     login me
@@ -15,5 +15,32 @@ RSpec.describe 'User Profile' do
     visit user_path me
 
     expect(page).to have_link('Change password')
+  end
+
+  xscenario "they can edit their information" do
+    user = User.create(first_name: "OriginalFirst",
+                       last_name:  "OriginalLast",
+                       email:      "Original@Email",
+                       twitter:    "OriginalTwitter",
+                       linked_in:  "OriginalLinkedIn",
+                       git_hub:    "OriginalGitHub",
+                       slack:      "OriginalSlack",
+                       cohort:     "OriginalCohort",
+                       password:   "OriginalPassword")
+    login user
+
+    visit user_path user
+    click_on "Edit profile"
+    fill_in "first_name", with: "NewFirst"
+
+    expect(user.first_name).to eq("NewFirst")
+    expect(user.last_name).to  eq("NewLast")
+    expect(user.email).to      eq("New@Email")
+    expect(user.twitter).to    eq("NewTwitter")
+    expect(user.linked_in).to  eq("NewLinkedIn")
+    expect(user.git_hub).to    eq("NewGitHub")
+    expect(user.slack).to      eq("NewSlack")
+    expect(user.cohort).to     eq("NewCohort")
+    expect(user.password).to   eq("NewPassword")
   end
 end
