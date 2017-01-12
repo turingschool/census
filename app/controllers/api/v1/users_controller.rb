d0533855dc1e72539d42cf1e6cb3e996b1f23211
PlanetEfficacy <jspevack@gmail.com>
2017-01-12 10:17:52 -0700

    Add feedback to user for answer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,28 @@
 class Api::V1::UsersController < Api::V1::ApiController
   # before_action :doorkeeper_authorize!
-  # before_filter :cors_preflight_check
-  # after_filter :cors_set_access_control_headers
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   def index
     render json: User.all, root_url: root_url, status: 200
   end
 
-  # private
-  #
-  # def cors_set_access_control_headers
-  #   headers['Access-Control-Allow-Origin'] = '*'
-  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  #   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-  #   headers['Access-Control-Max-Age'] = "1728000"
-  # end
-  #
-  # def cors_preflight_check
-  #   if request.method == 'OPTIONS'
-  #     headers['Access-Control-Allow-Origin'] = '*'
-  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  #     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-  #     headers['Access-Control-Max-Age'] = '1728000'
-  #     render :text => '', :content_type => 'text/plain'
-  #   end
-  # end
+  private
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 end
