32a7a2422cd108b80bbd2687cc32c9c923cfa542
PlanetEfficacy <jspevack@gmail.com>
2017-01-12 14:10:13 -0700

    Add doorkeeper to users controller api endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::UsersController < Api::V1::ApiController
-  # before_action :doorkeeper_authorize!
+  # before_action :testing
+  before_action :doorkeeper_authorize!
 
   def index
     render json: User.all, root_url: root_url, status: 200
   end
+  #
+  # def testing
+  #   byebug
+  # end
 end
