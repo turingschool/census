a331634291713d118cb15470544708411b4a83bc
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-02 18:05:12 -0700

    Update roles factory and begin admin:roles spec

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :role do
-    name "default"
+
+    sequence(:name, (%w[invited enrolled admin enrolled exited enrolled mentor graduated removed mentor]).cycle) do |name|
+      "#{name}"
+    end
+
   end
 end
---
+++ spec/features/admins/admin_manages_roles_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+# describe "admin manages user roles" do
+#   before :each do
+#     role1, role2, role3 = create_list(:role, 3)
+#     admin = create :admin
+#     sign_in admin
+#   end
+#   context "admin visits admin dashboard" do
+#     it "links to role index" do
+#       visit admin_dashboard_path
+#       click_on "Manage Roles"
+#
+#       expect(current_path).to eq(roles_path)
+#     end
+#   end
+#
+#   context "admin visits role index" do
+#     it "displays all roles" do
+#       visit roles_path
+#
+#       expect(page).to
+#     end
+#   end
+# end
