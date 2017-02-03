9e438d15f36d6a6e184e6d102f50866713930cd2
Brad <bgreen3@uoregon.edu>
2017-02-02 21:57:00 -0700

    Add test for single user endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < Api::V1::ApiController
-  # before_action :testing
   before_action :doorkeeper_authorize!
 
   def index
     render json: User.all, root_url: root_url, status: 200
   end
-  #
-  # def testing
-  #   byebug
-  # end
+
+  def show
+    render json: User.find(1), status: 200
+  end
+
 end
--- spec/requests/api/v1/users/users_request_spec.rb
+++ spec/requests/api/v1/users/users_request_spec.rb
@@ -34,4 +34,24 @@ RSpec.describe Api::V1::UsersController do
       expect(response_users.last["id"]).to eq(users.last.id)
     end
   end
+  context "Request for user by id is sent _with_ authorization credentials" do
+    it "returns info for all users" do
+      test_root_url = "http://www.example.com/"
+      users = create_list(:user, 2)
+      token = create(:access_token, resource_owner_id: users.first.id).token
+
+      get "/api/v1/users/#{users.first.id}", params: {access_token: token}
+      user = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(user.count).to eq(1)
+
+      expect(user["first_name"]).to eq(users.first["first_name"])
+      expect(user["last_name"]).to eq(users.first["last_name"])
+      expect(user["cohort"]["id"]).to eq(users.first["cohort_id"])
+      expect(user["image_url"]).to eq(test_root_url + users.first.image.url)
+      expect(user["id"]).to eq(users.first.id)
+
+    end
+  end
 end
