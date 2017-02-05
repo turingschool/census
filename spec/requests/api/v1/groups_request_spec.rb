5f389ec488cdc442d4035ab98656d3760ba835cf
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-04 23:18:32 -0700

    Add group request spec for update and delete Update routes Add groups controller API actions

--- spec/requests/api/v1/groups_request_spec.rb
+++ spec/requests/api/v1/groups_request_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe "Groups API" do
     it "updates and returns 204" do
       user = create(:user)
       token = create(:access_token, resource_owner_id: user.id).token
-      group = create(:group)
+      original_group = create(:group)
 
       headers = {"CONTENT-TYPE" => "application/json"}
-      params = {group: {id: group.id, name: "UpdatedName"}, access_token: token}.to_json
+      params = {group: {id: original_group.id, name: "UpdatedName"}, access_token: token}.to_json
 
-      patch "/api/v1/roles/#{group.id}", params: params, headers: headers
+      patch "/api/v1/groups/#{original_group.id}", params: params, headers: headers
 
-      role = JSON.parse(response.body)
+      group = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
-      expect(role['name']).to eq("UpdatedName")
-      expect(role['id']).to eq("#{group.id}")
+      expect(group['name']).to eq("UpdatedName")
+      expect(group['id']).to eq(original_group.id)
     end
 
   end
@@ -35,7 +35,7 @@ RSpec.describe "Groups API" do
       role = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(role['id']).to eq("#{group.id}")
+      expect(role['id']).to eq(group.id)
       expect(role['name']).to eq("#{group.name}")
     end
   end
