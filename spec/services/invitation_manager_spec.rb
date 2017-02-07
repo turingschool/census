09155e117f65f1dbf56caeaa133cf0f3315e043f
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-07 07:59:51 -0700

    student role assignment tested

--- spec/services/invitation_manager_spec.rb
+++ spec/services/invitation_manager_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe InvitationManager do
   before(:each) { create :role, name: "mentor" }
+  before(:each) { create :role, name: "active student" }
+  before(:each) { create :role, name: "enrolled" }
+  before(:each) { create :role, name: "graduated" }
   before(:each) { create :cohort, name: "1608-BE" }
 
   let(:invitation_params) do
@@ -99,6 +102,33 @@ RSpec.describe InvitationManager do
 
     expect(manager.status).to eq(:error)
     expect(manager.success?).to eq(false)
+  end
+
+  it "sets role to enrolled if student and unstarted cohort" do
+    params = { email: "good@example.com", role: "Student", cohort: "1703-FE" }
+    cohort = create(:cohort, name: "1703-FE", status: "unstarted")
+    manager = InvitationManager.new(params, user, url)
+    invite = Invitation.first
+
+    expect(invite.role.name).to eq("enrolled")
+  end
+
+  it "sets role to active student if student and active cohort" do
+    params = { email: "good@example.com", role: "Student", cohort: "1703-FE" }
+    cohort = create(:cohort, name: "1703-FE", status: "active")
+    manager = InvitationManager.new(params, user, url)
+    invite = Invitation.first
+
+    expect(invite.role.name).to eq("active student")
+  end
+
+  it "sets role to graduated if student and finished cohort" do
+    params = { email: "good@example.com", role: "Student", cohort: "1703-FE" }
+    cohort = create(:cohort, name: "1703-FE", status: "finished")
+    manager = InvitationManager.new(params, user, url)
+    invite = Invitation.first
+
+    expect(invite.role.name).to eq("graduated")
   end
 
   it "returns a notice if all invitations are created" do
