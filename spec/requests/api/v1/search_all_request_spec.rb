010c263eac1b7ec196cf8796b5a9b5e5096f088a
Brad <bgreen3@uoregon.edu>
2017-02-07 14:22:59 -0700

    Add test for cohort search endpoint

--- spec/requests/api/v1/search_all_request_spec.rb
+++ spec/requests/api/v1/search_all_request_spec.rb
@@ -13,14 +13,21 @@ RSpec.describe "General Search API" do
       params = {q: cohort_1.name}
       get "/api/v1/users/search_all", params: params
 
-      users = JSON.parse(response.body)
+      json_users = JSON.parse(response.body)
 
-      expect(users.count).to eq(2)
-      expect(users.include?(users.first.first_name)).to be_truthy
-      expect(users.include?(users.third.first_name)).to be_falsey
-      expect(users.include?(users.first.last_name)).to be_truthy
-      expect(users.include?(users.third.last_name)).to be_falsey
-      expect(users.first.group).to eq(group.name)
+      expect(json_users.count).to eq(2)
+
+      first = json_users.any? do |user|
+        user["first_name"] == users.first.first_name
+      end
+
+      last = json_users.any? do |user|
+        user["last_name"] == users.first.last_name
+      end
+
+      expect(first).to be_truthy
+      expect(last).to be_truthy
+      expect(json_users.first["groups"].first).to eq(group.name)
 
     end
   end
