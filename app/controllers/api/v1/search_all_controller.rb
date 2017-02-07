a965027b8c02eed85451cb21c09e65288b92b53b
Brad <bgreen3@uoregon.edu>
2017-02-07 14:41:47 -0700

    Add search endpoint for roles

--- app/controllers/api/v1/search_all_controller.rb
+++ app/controllers/api/v1/search_all_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::SearchAllController < Api::V1::ApiController
+  ROLES = ["applicant", "invited", "enrolled", "active student", "on leave",
+          "graduated", "exited", "removed", "mentor", "admin"]
+
   def index
     if params["q"].count("0-9") > 0
       cohort = Cohort.find_by(name: params[:q])
       users = cohort.users
+    elsif ROLES.include?(params["q"])
+      roles = Role.find_by(name: params[:q])
+      users = roles.users
     end
     render json: users
   end
--- spec/requests/api/v1/search_all_request_spec.rb
+++ spec/requests/api/v1/search_all_request_spec.rb
@@ -31,4 +31,34 @@ RSpec.describe "General Search API" do
 
     end
   end
+  context  "GET api/v1/users/search" do
+    it "searches by role and returns users with first, last and groups" do
+      role_1, role_2 = create_list(:role, 2)
+      users = create_list(:user, 3)
+      users.first.roles << role_1
+      users.second.roles << role_1
+      users.third.roles << role_2
+      group = create(:group, users: users)
+
+      params = {q: role_1.name}
+      get "/api/v1/users/search_all", params: params
+
+      json_users = JSON.parse(response.body)
+
+      expect(json_users.count).to eq(2)
+
+      first = json_users.any? do |user|
+        user["first_name"] == users.first.first_name
+      end
+
+      last = json_users.any? do |user|
+        user["last_name"] == users.first.last_name
+      end
+
+      expect(first).to be_truthy
+      expect(last).to be_truthy
+      expect(json_users.first["groups"].first).to eq(group.name)
+
+    end
+  end
 end
