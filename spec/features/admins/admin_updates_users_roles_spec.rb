e87e58ce39062a602ea7e5678355d16b0fdb6516
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-08 14:37:33 -0700

    Add feature test for admin updates roles on user

---
+++ spec/features/admins/admin_updates_users_roles_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Admin updates users' roles" do
+  context "they visit admin/roles/users" do
+    it "displays a form for updating users" do
+      role_1, role_2, role_3 = create_list(:role, 3)
+      users = create_list(:users, 3)
+      users.first.roles << role_1
+      users.second.roles << role_1
+      users.second.roles << role_2
+
+      visit edit_admin_roles_users_path
+
+      expect(page).to have_content("Find")
+      expect(page).to have_css('#search-box')
+      expect(page).to have_content("Add Role(s)")
+      expect(page).to have_content(role_1.name)
+      expect(page).to have_content(role_2.name)
+      expect(page).to have_content(role_3.name)
+      expect(page).to have_button('Save')
+    end
+  end
+end
