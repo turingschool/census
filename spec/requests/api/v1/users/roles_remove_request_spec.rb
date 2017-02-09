481f26764ec9facb19fc741fd90afc329e6810a5
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-08 19:41:14 -0700

    Add users/remove_roles spec

--- spec/requests/api/v1/users/roles_remove_request_spec.rb
+++ spec/requests/api/v1/users/roles_remove_request_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "User Roles API" do
-  context  "PATCH api/v1/users/add_roles" do
+  context  "PATCH api/v1/users/remove_roles" do
     it "updates and returns 200" do
       role_1, role_2 = create_list(:role, 2)
-      user_1, user_2 = create_list(:user, 2, roles: [role_1])
+      user_1, user_2 = create_list(:user, 2, roles: [role_1, role_2])
 
       headers = {"CONTENT-TYPE" => "application/json"}
       params = {roles: [role_2.id], users: [user_1.id, user_2.id]}.to_json
