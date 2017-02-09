bd18b0f129598b14bc2e5f474659dc72c1fcad17
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-08 19:58:10 -0700

    Make users/add_roles spec more robust

--- spec/requests/users_roles_request_spec.rb
+++ spec/requests/users_roles_request_spec.rb
@@ -12,11 +12,16 @@ RSpec.describe "User Roles API" do
       patch "/api/v1/users/add_roles", params: params, headers: headers
 
       users = JSON.parse(response.body)
-      
+
+      roles_present = false
+      roles_present = users.all? do |user|
+        user['roles'].any? do |role|
+          role['id'] == role_2.id
+        end
+      end
 
       expect(response).to have_http_status(200)
-
-
+      expect(roles_present).to eq(true)
     end
   end
 end
