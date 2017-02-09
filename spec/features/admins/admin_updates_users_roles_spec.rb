7aaeb06c9aa5920deb46af1653f988b51cca5f01
Brad <bgreen3@uoregon.edu>
2017-02-08 19:11:41 -0700

    Fix failing test

--- spec/features/admins/admin_updates_users_roles_spec.rb
+++ spec/features/admins/admin_updates_users_roles_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Admin updates users' roles" do
       visit admin_roles_users_edit_path
 
       expect(page).to have_content("Find")
-      expect(page).to have_css('#search-box')
+      expect(page).to have_css('#searchBox')
       expect(page).to have_content("Add Role(s)")
       expect(page).to have_content(role_1.name)
       expect(page).to have_content(role_2.name)
