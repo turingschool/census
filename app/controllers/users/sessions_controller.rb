3270eb98c94a842c8681647dc107bc3e71a2932f
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-15 22:40:23 -0700

    Pull log out function out of devise, set flash to success

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -21,7 +21,11 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
   def destroy
-    super
+    # super
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :success, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
   end
 
   # protected
