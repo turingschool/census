82c4f4eb63f140ecbf3de4d2ddb95b20b7af0e34
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-16 19:59:21 -0700

    Refactor filter by cohort to show 'All Users'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
   def index
     @cohorts = Cohort.all
     if params[:cohort] != '' && params[:cohort]
-      cohort = Cohort.find(params[:cohort])
-      @users = cohort.users
+      @cohort = Cohort.find(params[:cohort])
+      @users = @cohort.users
+      @selected = @cohort.name
     else
+      @selected = 'All Users'
       @users = User.all
     end
   end
