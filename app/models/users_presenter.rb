5f6bf5b6c113874c2d79b4301597ef5c9806a295
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-23 11:57:45 -0700

    Refactor UsersPresenter

--- app/models/users_presenter.rb
+++ app/models/users_presenter.rb
@@ -1,17 +1,44 @@
 class UsersPresenter
 
-  attr_reader :cohorts, :users, :selected
+  # attr_reader  :selected
   def initialize(cohort_id = nil)
-    @cohorts = Cohort.all
-    if cohort_id != '' && cohort_id
-      cohort = Cohort.find(cohort_id)
-      @users = cohort.users
-      @selected = cohort.name
+    @cohort_id = cohort_id
+    @all_selected = 'All Users'
+    @all_users = User.all
+
+    # if cohort_id != '' && cohort_id
+    #   cohort = Cohort.find(cohort_id)
+    #   # @users = cohort.users
+    #   @selected = cohort.name
+    # else
+    #   @selected = 'All Users'
+    #   # @users = User.all
+    # end
+  end
+
+  def cohorts
+    Cohort.all
+  end
+
+  def users
+    if @cohort_id != '' && @cohort_id
+      cohort = Cohort.find(@cohort_id)
+      cohort.users
     else
-      @selected = 'All Users'
-      @users = User.all
+      @all_users
     end
   end
+
+  def selected
+    if @cohort_id != '' && @cohort_id
+      cohort = Cohort.find(@cohort_id)
+      cohort.name
+    else
+      @all_selected
+    end
+  end
+
+
 
 
 end
