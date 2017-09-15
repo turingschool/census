4159634f3d28c8f938985037d814d83b7c9e74ef
Ali Schlereth <aschlere@sbcglobal.net>
2017-09-15 13:34:43 -0600

    Add migration gender pronoun column on user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223005124) do
+ActiveRecord::Schema.define(version: 20170915191929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20170223005124) do
     t.datetime "image_updated_at"
     t.integer  "cohort_id"
     t.string   "stackoverflow"
+    t.string   "gender_pronouns"
     t.index ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
