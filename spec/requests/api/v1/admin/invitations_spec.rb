6e15618ec12dd8b4eb071f18e2ee926cb7f6a6d6
Simon Taranto <simon.taranto@gmail.com>
2018-04-18 12:15:16 -0600

    Properly scope the API Admin controller
    We were getting CSRF errors once deployed when calling the invitations
    create API, which didn't make sense. Turned out the
    InvitationsController had been namespaced incorrectly to the HTML admin
    controller as opposed to the API admin controller. This issue should
    properly auth as well as fix the csrf issue.
    
    Follow on from
    https://trello.com/c/xVljwMcC/257-api-for-creating-an-invite-to-census-enroll

--- spec/requests/api/v1/admin/invitations_spec.rb
+++ spec/requests/api/v1/admin/invitations_spec.rb
@@ -13,9 +13,33 @@ RSpec.describe Api::V1::Admin::InvitationsController do
     end
   end
 
+  context "Request is sent _with_ non-admin authorization credentials" do
+    it "returns a 403 (unauthorized) response status" do
+      user = create :user
+      application = create(
+        :application,
+        owner: user
+      )
+      token = create(
+        :access_token,
+        resource_owner_id: user.id,
+        application: application
+      ).token
+      invitee_email = "invitee@example.com"
+
+      expect {
+        post api_v1_admin_invitations_path, params: {
+          access_token: token,
+          invitation:  { email: invitee_email }
+        }
+      }.not_to change { Invitation.where(email: invitee_email).count }
+
+      expect(response.status).to eq 403
+    end
+  end
+
   context "Request is sent _with_ authorization credentials" do
     it "creates the invitation" do
-      user_cohort = Cohort.new(OpenStruct.new(id: 1234, name: "1609-be"))
       user = create :user
       user.roles << Role.find_or_create_by!(name: "admin")
       application = create(
