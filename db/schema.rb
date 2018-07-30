2f8204491396059f2554e35d531c8c77ccb94756
Simon Taranto <srt32@users.noreply.github.com>
2018-07-30 14:34:19 -0600

    Unique index invitations.email (#338)
    Why:
    
    * we check it at the model and the invitation flow may be making dups
    
    https://trello.com/c/YqKKKJ9v/415-add-unique-index-on-invitationsemail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_192027) do
+ActiveRecord::Schema.define(version: 2018_07_29_145051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_05_17_192027) do
     t.integer "cohort_id"
     t.string "name"
     t.index ["cohort_id"], name: "index_invitations_on_cohort_id"
+    t.index ["email"], name: "index_invitations_on_email", unique: true
     t.index ["role_id"], name: "index_invitations_on_role_id"
     t.index ["user_id"], name: "index_invitations_on_user_id"
   end
