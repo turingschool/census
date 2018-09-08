bc2885c014f9a9cf0ed9491da086d065451e4026
Sal Espinosa <sespinos@gmail.com>
2018-09-08 13:56:01 -0600

    Remove duplicative call

--- app/controllers/api/v1/users/by_name_controller.rb
+++ app/controllers/api/v1/users/by_name_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Users::ByNameController < Api::V1::ApiController
   before_action :doorkeeper_authorize!
 
   def index
-    users = User.with_serializer_info.search_by_name(params[:q]).with_serializer_info
+    users = User.with_serializer_info.search_by_name(params[:q])
 
     render json: users, each_serializer: UserSerializer, root_url: root_url, status: 200
   end
