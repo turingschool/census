27bf444cfb55b3d0e0bf387666b65dfe252303e7
Sal Espinosa <sespinos@gmail.com>
2018-09-08 13:52:52 -0600

    Remoe n+1 queries with includes

--- app/controllers/api/v1/users/by_name_controller.rb
+++ app/controllers/api/v1/users/by_name_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Users::ByNameController < Api::V1::ApiController
   before_action :doorkeeper_authorize!
 
   def index
-    users = User.search_by_name(params[:q])
+    users = User.with_serializer_info.search_by_name(params[:q]).with_serializer_info
 
     render json: users, each_serializer: UserSerializer, root_url: root_url, status: 200
   end
